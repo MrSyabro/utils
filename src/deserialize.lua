840432f6597d6607b4f15becd4e4e0b90f60976e
MrSyabro <syabro1998@gmail.com>
2023-05-10 12:16:25 +0300

    Поменял окружение для десериализации

--- src/deserialize.lua
+++ src/deserialize.lua
@@ -1,15 +1,26 @@
 local M = {}
 
+local env = {
+	string = string,
+	math = math,
+	os = {
+		getenv = os.getenv,
+		time = os.time,
+		date = os.date,
+		difftime = os.difftime,
+		tmpname = os.tmpname
+	},
+}
+env.__index = env
+
 ---Deserialize string
 ---@param str string serialized lua data
 ---@param ser_func boolean serialize function or not (access to load())
 ---@return table?
 ---@return string?
 function M.str(str, ser_func)
-	local f, err = load("return " .. str, "desrialize", "bt", {
-		string = string,
-		os = os,
-	})
+	local newenv = setmetatable({}, env)
+	local f, err = load("return " .. str, "desrialize", "t", newenv)
 	if not f then return nil, err end
 	local data = f()
 	return data
