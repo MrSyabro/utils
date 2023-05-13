c7bd60417f2edf9ce20245b264c3a181b243a0fa
MrSyabro <syabro1998@gmail.com>
2023-05-13 12:41:03 +0300

    Добавил небольшую простенькую библиотеку

---
+++ src/data.lua
@@ -0,0 +1,28 @@
+local event = require "eventmgr"
+--[[
+		Функция создает таблицу, которая умеет сообщать об изменении данных внутри себя
+	получить событие можно в поле `data_changed: EventManager` и ествественно оно
+	защищено от записи.
+--]]
+
+---@return table
+return function()
+    local data = {
+        data_changed = event("Data_changed"),
+        __metatable = "aboba",
+    }
+    data.__index = data
+    data.__newindex = function (self, key, new_data)
+        if key == "__index"
+        or key == "__newindex"
+        or key == "__metatable"
+        or key == "data_changed"
+        then return end
+
+        self.data_changed(key, new_data)
+        data[key] = new_data
+    end
+    local obj = {}
+
+    return setmetatable(obj, data)
+end
