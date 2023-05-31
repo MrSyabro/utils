b7aeb4624edf17f8d22f87b08f8dffa578b08968
MrSyabro <syabro1998@gmail.com>
2023-05-31 17:17:13 +0300

    Добавил поддержку coroutines в менеджер событий

--- src/eventmgr.lua
+++ src/eventmgr.lua
@@ -1,38 +1,57 @@
 local obj = require "obj"
 
 ---@class Event : Object
----@operator call(...):nil 
+---@operator call(...):nil
 ---@field n number #кол-во функций менеджера
 ---@field name string #имя менеджера для дебага
----@field callbacks table<function, boolean> #список функций колбеков
+---@field protected callback_fns table<function, boolean> #список функций колбеков
+---@field protected callback_ths table<thread, boolean> #список рутин колбеков
 ---Менеджер событий для реализации подписки и рассылки на несколько функций
 local eventmgr_class = obj:new "Event"
 eventmgr_class.name = "Test"
 eventmgr_class.enabled = true
 
 ---Добавляет функцию в список рассылки менеджера событий
----@param callback_fn function
-function eventmgr_class:addCallback(callback_fn)
-	assert(type(callback_fn) == "function", "Bad callback type")
-	self.callbacks[callback_fn] = true
+---@param callback function|thread
+function eventmgr_class:addCallback(callback)
+	local t = type(callback)
+	if t == "function" then
+		self.callback_fns[callback] = true
+	elseif t == "thread" then
+		self.callback_ths[callback] = true
+	else error("Bad callback type", 2) end
 end
 
 ---Удаляет функцию из списка рассылки менеджера событий
----@param callback_fn function
-function eventmgr_class:rmCallback(callback_fn)
-	assert(type(callback_fn) == "function", "Bad callback type")
-
-	self.callbacks[callback_fn] = nil
+---@param callback function|thread
+function eventmgr_class:rmCallback(callback)
+	local t = type(callback)
+	if t == "function" then
+		self.callback_fns[callback] = nil
+	elseif t == "thread" then
+		if coroutine.status(callback) == "dead" then error("Corutine already died", 2) end
+		self.callback_ths[callback] = nil
+	else error("Bad callback type", 2) end
 end
 
 ---Рассылает событие по списку рассылки
 ---@vararg any?
 function eventmgr_class:send(...)
 	if self.enabled then
-		for callback_fn in pairs(self.callbacks) do
+		for callback_fn in pairs(self.callback_fns) do
 			local state, errmsg = xpcall(callback_fn, debug.traceback, ...)
 			if not state then
 				warn(self.name, " Callback error: ", errmsg)
+			end
+		end
+
+		for callback_th in pairs(self.callback_ths) do
+			local state, errmsg = coroutine.resume(callback_th, ...)
+			if not state then
+				warn(self.name, " Callback error: ", errmsg or "in coroutine")
+				self.callback_ths[callback_th] = nil
+			elseif coroutine.status(callback_th) == "dead" then
+				self.callback_ths[callback_th] = nil
 			end
 		end
 	end
@@ -48,9 +67,11 @@ return function(name, weak)
 	mgr.name = name
 	if weak then
 		mgr.__mode = "k"
-		mgr.callbacks = setmetatable({}, mgr)
+		mgr.callback_fns = setmetatable({}, mgr)
+		mgr.callback_ths = setmetatable({}, mgr)
 	else
-		mgr.callbacks = {}
+		mgr.callback_fns = {}
+		mgr.callback_ths = {}
 	end
 
 	return mgr
