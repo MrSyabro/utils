fc511d4b60f3053a158677ba2a5783d255ea0d5b
MrSyabro <syabro1998@gmail.com>
2023-06-05 23:38:27 +0300

    Добавил трансформы для vec2

--- src/transform.lua
+++ src/transform.lua
@@ -1,32 +1,49 @@
 local vec = require "vec"
 local M = {}
 
----Переводит угловой вектор направления в декартовый
+---Переводит угловой vec2 в декартовый vec3
 ---@param v number[]
 ---@return vec
 function M.rad2dec(v)
 	local sin2 = math.sin(v[2])
-	local dir = setmetatable({
+	local dir = vec(
 		sin2 * math.cos(v[1]),
 		sin2 * math.sin(v[1]),
-		math.cos(v[2]),
-	}, vec)
+		math.cos(v[2]))
 
 	return dir
 end
 
----Переводит декартовый вектор направления в угловой
+---Переводит декартовый vec3 в угловой vec2
 ---@param v number[]
 ---@return vec angles
 ---@return number len
 function M.dec2rad(v)
 	local len = vec.len(v)
-	local out = setmetatable({
+	local out = vec(
 		math.atan(v[1], v[2]),
 		math.atan(len, v[3])
-	}, vec)
+	)
 
 	return out, len
+end
+
+---Перводит угол в декартовый vec2
+---@param a number
+---@return vec
+function M.v2rad2dec(a)
+	local out = vec(math.cos(a), math.sin(a))
+
+	return out
+end
+
+---Переводит декартовый vec2 в угол
+---@param v number[]
+---@return number
+function M.v2dec2rad(v)
+	local out = math.atan(v[1], v[2])
+
+	return out
 end
 
 return M
