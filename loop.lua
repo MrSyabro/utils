fb858566dcb1ea46ef4d73031181767af11e9a16
MrSyabro <syabro1998@gmail.com>
2024-02-23 01:53:08 +0200

    feat: божественный loop:acall feat: божественный loop:awrap

--- loop.lua
+++ loop.lua
@@ -1,6 +1,7 @@
 local obj = require "obj"
 
 local cr, cs = coroutine.resume, coroutine.status
+local tp, tu = table.pack, table.unpack
 
 ---@class Loop : Object
 ---@field pool table<thread, boolean>
@@ -27,6 +28,35 @@ end
 function loopclass:register(thread)
     if type(thread) ~= "thread" then error("Bad thread type", 2) end
     self.pool[thread] = true
+end
+
+---Вызывает функцию с аргументами обернув в рутину. Возвращает результат первого вызова resume
+---@param func function
+---@param ... any
+---@return boolean success успешен ли первый вызов resume
+---@return any ...
+function loopclass:acall(func, ...)
+	local newth = coroutine.create(func)
+	local out = tp(cr(newth, ...))
+	if out[1] then
+		if cs(newth) == "suspended" then
+			self.pool[newth] = true
+		end
+		table.remove(out, 1)
+		return tu(out)
+	else
+		return false, out[2]
+	end
+end
+
+---Функция аналогична coroutine.wrap, возращает, которая вызовет loop:acall(func)
+---@param func function
+---@param ... any
+---@return function wrap
+function loopclass:awrap(func, ...)
+	return function(...)
+		return self:acall(func, ...)
+	end
 end
 
 ---Создает новый пул нитей
