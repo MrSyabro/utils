c17a7e28c9477ea1f1f43888203e3cc5179034d2
MrSyabro <syabro1998@gmail.com>
2024-04-11 03:50:00 +0300

    Новая библиотека `relocsafetab`
    Новая библиотека призвана унифицировать решение для прохода по динамичным таблицам, которые могут измениться во время `pairs`

---
+++ relocsafetab.lua
@@ -0,0 +1,42 @@
+local mt = {
+	__index = function(pool, key)
+		return pool[true][key] or pool[false][key]
+	end,
+	__newindex = function(pool, key, data)
+		local next = pool.next
+		local p = pool[not next]
+		if p[key] then
+			p[key] = data
+		else
+			pool[next][key] = data
+		end
+	end,
+	__pairs = function(pool)
+		local cur = true
+		pool.next = false
+		return function(pool, i)
+			local nkey, ndata = next(pool[cur], i)
+			if nkey then
+				return nkey, ndata
+			elseif cur == false then
+				return nil
+			else
+				pool.next = true
+				cur = false
+				return next(pool[cur])
+			end
+		end, pool
+	end
+}
+
+---Создает таблицу, которая позволяет добавлять элементы вовремя `pairs`
+---@return table<any, any>
+local function relocatesafetytable()
+	return setmetatable({
+		[true] = {},
+		[false] = {},
+		next = false
+	}, mt)
+end
+
+return relocatesafetytable
